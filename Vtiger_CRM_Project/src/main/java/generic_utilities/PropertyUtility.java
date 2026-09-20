61daa0cb415ffa44a2c0dfe686e25dc18782e2f6
Kabir99-login <kabirpratapsingh99@gmail.com>
2026-09-20 18:20:24 +0530

    Implement readPropertyFile method in PropertyUtility
    Added method to read properties from a file.

--- Vtiger_CRM_Project/src/main/java/generic_utilities/PropertyUtility.java
+++ Vtiger_CRM_Project/src/main/java/generic_utilities/PropertyUtility.java
@@ -2,4 +2,11 @@ package generic_utilities;
 
 public class PropertyUtility {
 
+  public String readPropertyFile(String key){
+    FileInputStream fis = new FileInputStream();
+    Properties p= new Properties();
+    p.load(fis);
+    return p.getProperty(key);
+  }
+
 }
